27919d7ad030575620ba07f35148dcb0dc8006b8
Marcus Portmann <marcus@mmp.guru>
2018-04-23 23:06:46 +0200

    Updates

--- src/inception-sms/src/test/java/digital/inception/sms/SMSServiceTest.java
+++ src/inception-sms/src/test/java/digital/inception/sms/SMSServiceTest.java
@@ -55,6 +55,6 @@ public class SMSServiceTest
   public void test()
     throws Exception
   {
-    smsService.sendSMSSynchronously(1, "0832763107", "Testing 1.. 2.. 3..");
+    smsService.sendSMSSynchronously(1, "0832763107", "Testing at 23:02...");
   }
 }
