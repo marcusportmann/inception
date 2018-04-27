c725076ebc22a8b35ad5d45c20e15a3eea91fbbb
Marcus Portmann <marcus@mmp.guru>
2018-04-27 22:35:35 +0200

    Updates

---
+++ src/inception-sample/src/main/frontend/src/app/inception/components/layout/error-modal/error-modal.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit, ViewChild} from '@angular/core';
+
+
+@Component({
+  selector: 'error-modal',
+  templateUrl: './error-modal.component.html'
+})
+export class ErrorModalComponent implements OnInit {
+
+  public errorModal;
+
+  ngOnInit(): void {
+  }
+
+
+}
