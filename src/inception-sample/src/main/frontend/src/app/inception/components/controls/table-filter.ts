c80f9d91bf65c5afa9ecc8303ff56f0d4ccdd977
Marcus Portmann <marcus@mmp.guru>
2018-08-12 08:48:43 +0200

    Updates

---
+++ src/inception-sample/src/main/frontend/src/app/inception/components/controls/table-filter.ts
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2018 Marcus Portmann
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output
+} from '@angular/core';
+
+@Component({
+  selector: 'table-filter',
+  template: `
+
+    <div class="table-filter-container">
+      <div class="table-filter-icon">
+        <i class="fa fa-search"></i>
+      </div>
+      <input class="table-filter-input" matInput [value]="this.value"  #tableFilterInput (keyup)="updateValue($event.target.value)" placeholder="Search..." autocomplete="off">
+      <button class="table-filter-reset" mat-icon-button *ngIf="value" (click)="resetValue()">
+        <i class="fa fa-times"></i>
+      </button>
+    </div>
+  `,
+  styles: [`
+
+    .table-filter-container {
+      display: flex; 
+      align-items: center; 
+      border: 1px solid rgba(0, 0, 0, 0.12); 
+      border-radius: 2px;  
+    }
+    
+    .table-filter-icon {
+      display: flex;
+      width: 28px;
+      height: 28px;
+      align-items: center; 
+      color: rgba(0, 0, 0, 0.42); 
+      justify-content: center; 
+      border: none;
+    }
+    
+    .table-filter-input {
+      -webkit-appearance: none; 
+      border-radius: 0; 
+      width: 12em; 
+      height: 28px; 
+      padding-left: 2px; 
+      padding-right: 28px; 
+      border: none; 
+      background-color: transparent;
+    }
+    
+    .table-filter-reset {
+      position: absolute; 
+      right: 16px; 
+      color: rgba(0, 0, 0, 0.42); 
+      width: 28px; height: 28px; 
+      line-height: 28px; 
+      min-width: 28px; 
+      padding: 0; 
+      border: none !important;
+    }
+
+  `]
+})
+export class TableFilter {
+
+  @Input()
+  value: string = '';
+
+  @Output()
+  changed: EventEmitter<string> = new EventEmitter<string>();
+
+  resetValue(): void {
+    this.value = '';
+    this.changed.emit(this.value);
+  }
+
+  updateValue(newValue: string): void {
+    this.value = newValue;
+    this.changed.emit(this.value);
+  }
+}
