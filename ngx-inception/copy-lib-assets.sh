fe0dae808433d023e55c02d0f0614fdfdc019063
Marcus Portmann <marcus@mmp.guru>
2019-12-08 09:24:20 +0200

    Updates

--- ngx-inception/copy-lib-assets.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo "-------------------------------------------------"
-echo "Copying assets to library dist folder."
-echo "-------------------------------------------------"
-cp -avr ./projects/ngx-inception/src/assets ./dist/ngx-inception
