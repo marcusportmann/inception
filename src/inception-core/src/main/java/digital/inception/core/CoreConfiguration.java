db04a9173af57ff8839734553205581e81cfbc5f
Marcus Portmann <marcusp@discovery.co.za>
2025-02-09 15:52:21 +0200

    no-ref: Fixes

---
+++ src/inception-core/src/main/java/digital/inception/core/CoreConfiguration.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright Marcus Portmann
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package digital.inception.core;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.core.convert.support.DefaultConversionService;
+
+/** The {@code CoreConfiguration} class provides the Spring configuration for the Core module. */
+@Configuration
+public class CoreConfiguration {
+
+  /** The Spring {@link ApplicationContext}. */
+  private final ApplicationContext applicationContext;
+
+  /**
+   * Constructs a new {@code CoreConfiguration}.
+   *
+   * @param applicationContext the Spring {@link ApplicationContext}
+   */
+  public CoreConfiguration(ApplicationContext applicationContext) {
+    this.applicationContext = applicationContext;
+  }
+
+  /**
+   * Returns the Spring {@link ConversionService} that incorporates the custom converters that form
+   * part of the Nova Framework.
+   *
+   * @return the Spring {@link ConversionService} that incorporates the custom converters that form
+   *     part of the Nova Framework
+   */
+  @Bean
+  public ConversionService conversionService() {
+    DefaultConversionService conversionService = new DefaultConversionService();
+
+    return conversionService;
+  }
+}
