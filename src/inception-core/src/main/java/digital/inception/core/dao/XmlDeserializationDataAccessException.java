b49145d480f90faecaf5c627c27e873e0d42404d
Marcus Portmann <marcusp@discovery.co.za>
2025-11-18 23:02:19 +0200

    Updates

---
+++ src/inception-core/src/main/java/digital/inception/core/dao/XmlDeserializationDataAccessException.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright Marcus Portmann
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package digital.inception.core.dao;
+
+import org.springframework.dao.DataRetrievalFailureException;
+
+/**
+ * The {@code XmlDeserializationDataAccessException} exception is thrown to indicate a failure to
+ * deserialize XML during data access.
+ * 
+ * @author Marcus Portmann
+ */
+public class XmlDeserializationDataAccessException extends DataRetrievalFailureException {
+
+  /**
+   * Constructs a new {@code XmlDeserializationDataAccessException} with the specified message and
+   * cause.
+   *
+   * @param message The message saved for later retrieval by the {@code getMessage()} method.
+   * @param cause The cause saved for later retrieval by the {@code getCause()} method. (A {@code
+   *     null} value is permitted if the cause is nonexistent or unknown)
+   */
+  public XmlDeserializationDataAccessException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
